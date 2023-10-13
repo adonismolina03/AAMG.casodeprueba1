9078b1557896f593d738f4c67afabd18fcd7c231
adonismolinao3 <adonismolinao3@outlook.com>
2023-10-13 16:52:53 -0600

    Test terminado

---
+++ AAMG.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AAMG.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAMG.Temperatura.Tests
+{
+    [TestClass()]
+    public class TemperatureConverterTests
+    {
+        [TestMethod()]
+        public void CelciusToFarenheitTest()
+        {
+
+            TemperatureConverter converter = new TemperatureConverter();
+            double celsius = 25;
+
+            double result = converter.CelciusToFarenheit(celsius);
+
+            Assert.AreEqual(77, result);
+        }
+
+        [TestMethod()]
+        public void FarenheitToCelciusTest()
+        {
+            TemperatureConverter converter = new TemperatureConverter();
+            double fahrenheit = 77; 
+
+            
+            double result = converter.FarenheitToCelcius(fahrenheit);
+
+            
+            Assert.AreEqual(25, result); 
+        }
+    }
+}
